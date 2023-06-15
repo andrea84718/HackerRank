b8c1e60a60d0a59d288ec2ff2f14f3038f93594a
Andrea <andrea84718@gmail.com>
2023-06-15 12:43:01 +0100

    Create select all.sql

---
+++ SQL/Select/select all.sql
@@ -0,0 +1,21 @@
+/*
+
+Query all columns (attributes) for every row in the CITY table.
+
+The CITY table is described as follows:
+
+            CITY
++-------------+--------------+
+| Field       | Type         |
++-------------+--------------+
+| ID          | NUMBER       |
+| NAME        | VARCHAR2(17) |
+| COUNTRYCODE | VARCHAR2(2)  |
+| DISTRICT    | VARCHAR2(20) |
+| POPULATION  | NUMBER       |
++-------------+--------------+
+
+*/
+
+select *
+from CITY

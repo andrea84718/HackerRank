fda5b5456ff1a315b506917d97eb6ca94ae4f233
Andrea <andrea84718@gmail.com>
2023-06-22 10:53:04 +0100

    Create The World of Numbers.sh

---
+++ Linux Shell/Bash/The World of Numbers.sh
@@ -0,0 +1,37 @@
+<<COMMENT
+Given two integers, x and y, find their sum, difference, product, and quotient.
+
+Input Format
+Two lines containing one integer each (x and y, respectively).
+
+Constraints
+-100 <= x,y <= 100
+y != 0
+
+Output Format
+Four lines containing the sum (x+y), difference (x-y), product (x*y), and quotient (x/y), respectively.
+(While computing the quotient, print only the integer part.)
+
+Sample Input
+5
+2
+
+Sample Output
+7
+3
+10
+2
+
+Explanation
+5 + 2 = 7
+5 - 2 = 3
+5 * 2 = 10
+5 / 2 = 2 (Integer part)
+COMMENT
+
+read x
+read y
+echo $(($x + $y))
+echo $(($x - $y))
+echo $(($x * $y))
+echo $(($x / $y))

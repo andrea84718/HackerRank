9ee242a491086b5ff7ab4a42e915ac2af3c222b9
Andrea <andrea84718@gmail.com>
2023-06-22 10:35:03 +0100

    Create Looping and Skipping.sh

---
+++ Linux Shell/Bash/Looping and Skipping.sh
@@ -0,0 +1,28 @@
+/*
+Your task is to use for loops to display only odd natural numbers from  to .
+
+Input Format
+There is no input.
+
+Constraints
+-
+
+Output Format
+1
+3
+5
+.
+.
+.
+.
+.
+99  
+*/
+
+for ((i=1;i<=99;i++))
+    do 
+        if (((i%2)!=0))
+            then 
+                echo "$i"
+        fi
+    done

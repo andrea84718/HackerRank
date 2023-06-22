a3cf42aaa798ee438a979b8250c167d94007a0a3
Andrea <andrea84718@gmail.com>
2023-06-22 10:45:44 +0100

    Create Looping with Numbers.sh

---
+++ Linux Shell/Bash/Looping with Numbers.sh
@@ -0,0 +1,24 @@
+<<COMMENT
+Use a for loop to display the natural numbers from 1 to 50.
+
+Input Format
+There is no input
+
+Output Format
+1
+2
+3
+4
+5
+.
+.
+.
+.
+.
+50
+COMMENT
+
+for ((i=1;i<=50;i++))
+    do
+     echo "$i"
+    done
